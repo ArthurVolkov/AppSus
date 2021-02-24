9eb3c3f271ffc2e4c038b2b3fb5054fab09e6b8c
izekgolland <izek.go@gmail.com>
2021-02-24 23:43:18 +0200

    render keep

--- js/apps/keep/pages/keep-app.cmp.js
+++ js/apps/keep/pages/keep-app.cmp.js
@@ -1,6 +1,7 @@
 import noteTxt from '../cmps/note-text.cmp.js'
 import noteImg from '../cmps/note-img.cmp.js'
 import noteTodos from '../cmps/note-todo.cmp.js'
+import noteVideo from '../cmps/note-video.cmp.js'
 import { keepService } from '../sevices/keep.service.js'
 
 
@@ -8,7 +9,7 @@ export default {
     template: `
         <section class="keep-app">
             <div v-for="(keep, idx) in keeps">
-                <component :is="keep.type" :info="keep.info"></component>
+                <component :is="keep.type" :keep="keep"></component>
             </div>
         </section>
     `,
@@ -32,6 +33,7 @@ export default {
         noteTxt,
         noteImg,
         noteTodos,
+        noteVideo
     }
 };
 
