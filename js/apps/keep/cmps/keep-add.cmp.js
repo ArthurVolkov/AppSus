fee03c4f145e75244a333fec8e2f796fe3feb820
izekgolland <izek.go@gmail.com>
2021-02-26 00:59:57 +0200

    fixed inputs

--- js/apps/keep/cmps/keep-add.cmp.js
+++ js/apps/keep/cmps/keep-add.cmp.js
@@ -3,22 +3,14 @@ import { keepService } from '../sevices/keep.service.js'
 export default {
     template: `
     <section class="keep-add flex flex-col">
-
-
         <img v-if="curImage.imageUrl" :src="curImage.imageUrl" alt="image" />
-
         <div class="add-textarea-container flex flex-col justify-center">
             <ul v-if="isTodo" class="checkbox-container clean-list">
                 <li v-for="row in rowsCount">
                     <input type="checkbox" />
                 </li>
             </ul>
-            <!-- <textarea :rows="rowsCount"  v-model="keepToAddTxt"></textarea> -->
-
-
-            <input v-for="(row, idx) in keep.info.txts.length+2" :key="idx" :ref="idx" @keydown="newLine($event, idx)" type="text" v-model="keep[idx]" />
-
-
+            <input v-for="(row, idx) in keep.info.txts.length+1" :key="idx" :ref="idx" @keydown="newLine($event, idx)" type="text" v-model="keep.info.txts[idx]" />
         </div>
 
         <div class="add-input-container flex justify-around">
@@ -36,7 +28,6 @@ export default {
     data() {
         return {
             keep: null,
-            keepToAddTxt: '',
             // src: null,
             isImg: false,
             isVideo: false,
@@ -49,9 +40,15 @@ export default {
         }
     },
     computed: {
-        rowsCount() {
-            const lines = this.keepToAddTxt.split(/\r|\r\n|\n/);
-            return lines.length
+        rowsCount() {            
+            for (var i=0;i<this.keep.info.txts.length;i++){
+                if (this.keep.info.txts[i] === '') {
+                    this.keep.info.txts.splice(i,1)
+                    i--;
+                }
+            }
+            console.log(this.keep.info.txts);
+            return this.keep.info.txts.length;
         }
     },
     methods: {
@@ -62,14 +59,8 @@ export default {
             this.curImage.imageUrl = URL.createObjectURL(file)
         },
         addNewKeep() {
-            var lines = this.keepToAddTxt.split(/\r|\r\n|\n/);
-            for (var i = 0; i < lines.length; i++) {
-                this.keep.info.txts.push({ txt: lines[i], doneAt: null })
-            }
             // this.keep.info.url = this.src;
             this.keep.info.url = this.curImage.imageUrl;
-
-
             if (this.isImg) {
                 this.keep.type = "noteImg";
             } else if (this.isVideo) {
@@ -81,12 +72,25 @@ export default {
         toTodo() {
             this.isTodo = !this.isTodo
         },
+        //backspace 8 up 38 down 40
         newLine(ev, idx) {
             if (ev.which === 13) {
+                this.keep.info.txts.splice(idx+1,0,'')
                 idx = idx + 1 + '';
-                this.keep.info.txts.push('')
                 this.$refs[idx][0].focus()
             }
+            if (ev.which === 38) {
+                if (idx > 0) this.$refs[idx-1][0].focus()
+            }
+            if (ev.which === 40) {
+                if (idx < this.keep.info.txts.length-1) this.$refs[idx+1][0].focus()
+            }
+            if (ev.which === 8 && this.keep.info.txts[idx] === '' && idx > 0) {
+                this.keep.info.txts.splice(idx,1,)
+                idx = idx - 1 + '';
+                this.$refs[idx][0].focus()
+            }
+
         }
     },
     created() {
