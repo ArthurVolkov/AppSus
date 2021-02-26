316a02fa3e36fdd443736f778b7cc59f118effcb
Arthur-Volkov <bafequan@gmail.com>
2021-02-26 19:41:33 +0200

    fixes -Arthur

--- js/apps/keep/cmps/keep-list.cmp.js
+++ js/apps/keep/cmps/keep-list.cmp.js
@@ -3,13 +3,18 @@ import noteImg from './note-img.cmp.js'
 import noteTodos from './note-todo.cmp.js'
 import noteVideo from './note-video.cmp.js'
 import { eventBus } from "../../services/event-bus-service.js"
+import itemBtns from './keep-item-btns.cmp.js'
 import { keepService } from '../sevices/keep.service.js'
 
 export default {
     props: ['keeps'],
     template: `
             <ul class="keep-list clean-list">
-                <component v-for="(keep, idx) in keeps" :key="keep.id" :is="keep.type" :keep="keep" @click.native.stop="select(keep)" class="keep-item"></component>
+            <!-- <component v-for="(keep, idx) in keeps" :key="keep.id" :is="keep.type" :keep="keep" @click.native.stop="select(keep)" class="keep-item"></component> -->
+                <div v-for="(keep, idx) in keeps" :key="keep.id" class="keep-item">
+                    <component :is="keep.type" :keep="keep" @click.native.stop="select(keep)"></component>
+                    <item-btns></item-btns>
+                </div>
             </ul>
     `,
     data() {
@@ -29,7 +34,8 @@ export default {
         noteTxt,
         noteImg,
         noteTodos,
-        noteVideo
+        noteVideo,
+        itemBtns
     }
 };
 
