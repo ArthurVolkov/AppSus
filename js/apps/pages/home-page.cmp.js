6745ce73533f1253975f014f910b621bfe6ea37e
Arthur-Volkov <bafequan@gmail.com>
2021-02-28 03:12:46 +0200

    css fixes -Arthur

--- js/apps/pages/home-page.cmp.js
+++ js/apps/pages/home-page.cmp.js
@@ -1,7 +1,13 @@
 export default {
     template:`
-    <section class="home app-main flex justify-center align-center">
-        <h1>Home sweet home</h1>
+    <section class="home app-main flex flex-col justify-center align-center">
+        <h1 tag="h1" to="/" class="home-logo pointer">
+            <span>A</span><span>p</span><span>p</span><span>S</span><span>u</span><span>s</span>
+        </h1>
+        <div class="dark fire">
+		    <h1 class="Blazing">The new way of life</h1>
+	   </div>
+
     </section>
     `
 }
